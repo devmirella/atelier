68a62510ab7b32e53d6a1e8a34661b5048c00506
devmirella <marinasillva43@gmail.com>
2026-02-03 20:41:26 -0300

    Finaliza front-end base e layout de login com efeito memória

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/arte")
+def arte():
+    return render_template("arte.html")
+
+@app.route("/exposed")
+def exposed():
+    return render_template("exposed.html")
+
+@app.route("/inspiracoes")
+def inspiracoes():
+    return render_template("inspiracoes.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
