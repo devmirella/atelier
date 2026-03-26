3f138edb5201e8e11b3854808ddaaf49d841b0ea
devmirella <marinasillva43@gmail.com>
2026-03-25 23:49:45 -0300

    chore: remove arquivo temporário tornar_admin

--- tornar_admin.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from models import Usuario
-
-with app.app_context():
-    usuario = Usuario.query.filter_by(email="marinasillva43@gmail.com").first()
-    usuario.is_admin = True
-    db.session.commit()
-    print("Feito!")
